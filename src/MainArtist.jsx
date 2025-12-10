00b8e4bd2fc1d899cb0ad8d48245544c7f9c6775
Marcus <genestonmarcus@gmail.com>
2025-12-11 03:24:08 +0800

    Refactor main artist page to use reusable ArtistCards component
    Replaced individual divs for each artist with the ArtistCards component for LE SSERAFIM, AESPA, SEVENTEEN, TWICE, and TREASURE.
    Renamed CSS IDs as needed to support the new reusable component structure.

--- src/MainArtist.jsx
+++ src/MainArtist.jsx
@@ -4,6 +4,39 @@ import NavigationBar from "./NavigationBar";
 import { useNavigate } from "react-router-dom";
 import ArtistsContents from "./ArtistsContents.json"
 
+const ArtistCardImageStyle = (backgroundImage) => ({
+  backgroundImage: `
+    linear-gradient(
+      to bottom,
+      rgba(255,255,255,0),
+      rgb(0,0,0)
+    ), url(${backgroundImage})
+  `,
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+  backgroundPosition: "35% 35%",
+  width: "950px",
+  height: "650px",
+  textAlign: "center",
+  borderTopLeftRadius: "20px",
+  borderTopRightRadius: "20px",
+  margin: "0 auto",
+  display: "block",
+  color: "white",
+  fontFamily: '"Work Sans", sans-serif',
+  fontSize: "150%"
+});
+
+const ArtistCards = ({artistName, onClickButtonFunction, artistBackgroundImage}) => {
+    return (
+            <div id="main-artist-content">
+              <div style={ArtistCardImageStyle(artistBackgroundImage)} onClick={onClickButtonFunction}>
+                <div id="main-artist-group-name">{artistName}</div>
+              </div>
+            </div>
+    );
+  }
+
 const MainArtist = () => {
   const navigate = useNavigate();
 
@@ -80,35 +113,28 @@ const MainArtist = () => {
 
       <div id="main-artist-background">
         <div id="main-artist-gallery">
-          <div id="main-artist-content">
-            <div id="main-artist-lesserafim" onClick={leSserafimClick}>
-              <div id="main-artist-group-name-lesserafim">LE SSERAFIM</div>
-            </div>
-          </div>
 
-          <div id="main-artist-content">
-            <div id="main-artist-aespa" onClick={aespaClick}>
-              <div id="main-artist-group-name-aespa">aespa</div>
-            </div>
-          </div>
+        
+        <ArtistCards artistName={"LE SSERAFIM"}
+        onClickButtonFunction={leSserafimClick}
+        artistBackgroundImage="/images/lesserafim-main.webp"/>
 
-          <div id="main-artist-content">
-            <div id="main-artist-seventeen" onClick={seventeenClick}>
-              <div id="main-artist-group-name-seventeen">SEVENTEEN</div>
-            </div>
-          </div>
+        <ArtistCards artistName={"AESPA"} 
+        onClickButtonFunction={aespaClick}
+        artistBackgroundImage="/images/aespa-main.jfif"/>
 
-          <div id="main-artist-content">
-            <div id="main-artist-twice" onClick={twiceClick}>
-              <div id="main-artist-group-name-twice">TWICE</div>
-            </div>
-          </div>
+        <ArtistCards artistName={"SEVENTEEN"}
+        onClickButtonFunction={seventeenClick}
+        artistBackgroundImage="/images/seventeen-main.jpg"/>
 
-          <div id="main-artist-content">
-            <div id="main-artist-treasure" onClick={treasureClick}>
-              <div id="main-artist-group-name-treasure">TREASURE</div>
-            </div>
-          </div>
+        <ArtistCards artistName={"TWICE"}
+        onClickButtonFunction={twiceClick}
+        artistBackgroundImage="/images/twice-main.jpg"/>
+
+        <ArtistCards artistName={"TREASURE"}
+        onClickButtonFunction={treasureClick}
+        artistBackgroundImage="/images/treasure-main.jfif"/>
+        
         </div>
       </div>
     </div>
