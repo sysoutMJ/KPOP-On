2a72dba26c2d2ddb6c1e79c0aeb009324db0543c
Marcus <genestonmarcus@gmail.com>
2025-12-19 05:19:10 +0800

    Refactor(cart): make handleClick and handleChange React-safe and immutable
    Replace object reference checks with id comparison. This prevents direct
    state mutation.

--- src/App.jsx
+++ src/App.jsx
@@ -13,26 +13,74 @@ import Checkout from "./Checkout";
 import Thankyou from "./Thankyou";
 
 import Artist from "./Artist"
+
+
 function App() {
   const [cart, setCart] = useState([]);
 
-  const handleClick = (values) => {
-    if (cart.indexOf(values) !== -1) return;
-    setCart([...cart, values]);
+  // prev is previous state
+  // [...prev, item] creates a new array that contains everything in prev, plus item at the end
+  const addToCart = (item) => {
+    setCart(prev => [...prev, item]);
+  }
+
+  const removeFromCart = (id) => {
+    setCart(prev => prev.filter(i => i.id !== id));
   };
 
-  const handleChange = (values, d) => {
-    const ind = cart.indexOf(values);
-    const arr = cart;
-    arr[ind].amount += d;
+  // What it does:
+  //    - If this item is already in the cart, don’t add it again
+  //    - This prevents duplicate items to be added into cart
 
-    if (arr[ind].amount === 0) arr[ind].amount = 1;
-    setCart([...arr]);
+  // indexOf() checks if the exact same object reference exists in cart
+  // const handleClick = (values) => {
+  //   // values is object being passed based on my analyzation from the other .jsx
+  //   // values is {id, title, price, img} 
+  //   // object reference is the “address” of an object in memory.
+  //   // values is not primitive.
+  //   // If the exact same object reference exists. -1 here means the item does not exist.
+  //   // then return. do nothing. Else, create another array and add it at the end. 
+
+  //   // so in cart, indexof checks each. If the memory address of each of the checked 
+  //   // values(the item) matches the memory addresses that exists in the cart. Do nothing.
+  //   // Else, if the memory address of the passed values(the item) does not exist in the addreess
+  //   // that are in the cart. Create new array and add it.
+  //   if (cart.indexOf(values) !== -1) return;
+  //   setCart([...cart, values]);
+    
+  // };
+    const handleClick = (item) => {
+      setCart(prev => {
+        if (prev.find(i => i.id === item.id)) return prev;
+        return [...prev, { ...item, amount: 1 }];
+      });
+    };
+
+
+  // This function is for increasing and decreasing the item quantity
+  // const handleChange = (values, d) => {
+  //   // Finds the item index in the cart.
+  //   const ind = cart.indexOf(values);
+
+  //   const arr = cart;
+  //   arr[ind].amount += d;
+
+  //   if (arr[ind].amount === 0) arr[ind].amount = 1;
+  //   setCart([...arr]);
+  // };
+
+  const handleChange = (item, delta) => {
+    setCart(prev =>
+      prev.map(cartItem =>
+        cartItem.id === item.id
+          ? { ...cartItem, amount: Math.max(1, cartItem.amount + delta) }
+          : cartItem
+      )
+    );
   };
-
-  useEffect(() => {
-    console.log("cart change");
-  }, [cart]);
+  // useEffect(() => {
+  //   console.log("Use Effect Triggered ");
+  // }, [cart]);
 
   return (
     <>
@@ -50,6 +98,7 @@ function App() {
           <Route path="/AboutUs" element={<AboutUs />} />
           <Route path="/checkout" element={<Checkout cart={cart} />} />
           <Route path="/thankyou" element={<Thankyou />} />
+
           {/* for shops */}
           <Route
             path="/lesserafim"
